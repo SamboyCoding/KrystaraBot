93703c8ee3ca52ab15da1df0964e5371de67c164
Sam Byass <sam@whcuk.net>
2016-11-13 17:48:12 +0000

    Basic commands

--- src/main/java/me/samboycoding/krystarabot/Listener.java
+++ src/main/java/me/samboycoding/krystarabot/Listener.java
@@ -1,24 +1,21 @@
 package me.samboycoding.krystarabot;
 
-import java.awt.Color;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.ZoneId;
 import java.util.Date;
-import java.util.EnumSet;
-import java.util.List;
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.api.events.EventSubscriber;
+import sx.blah.discord.handle.impl.events.MessageReceivedEvent;
 import sx.blah.discord.handle.impl.events.ReadyEvent;
-import sx.blah.discord.handle.impl.obj.Role;
 import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IGuild;
-import sx.blah.discord.handle.obj.IRole;
-import sx.blah.discord.handle.obj.Permissions;
+import sx.blah.discord.handle.obj.IMessage;
+import sx.blah.discord.handle.obj.IUser;
 import sx.blah.discord.handle.obj.Status;
 import sx.blah.discord.util.DiscordException;
 import sx.blah.discord.util.MissingPermissionsException;
 import sx.blah.discord.util.RateLimitException;
-import sx.blah.discord.util.RoleBuilder;
 
 /**
  * Main event listener
@@ -44,7 +41,7 @@ public class Listener
             cl.changeStatus(Status.game("Made by SamboyCoding and MrSnake"));
             main.log("My ID: " + main.getClient(null).getApplicationClientID());
             IDReference.MYID = main.getClient(null).getApplicationClientID();
-            logToLogChannel("Bot started", main.getClient(null).getGuildByID(IDReference.SERVERID));
+            //logToLogChannel("Bot started", cl.getGuildByID(IDReference.SERVERID));
             main.log("Finished processing readyEvent. Bot is 100% up now.\n\n");
         } catch (Exception ex)
         {
@@ -67,14 +64,54 @@ public class Listener
             srv.getChannelByID(IDReference.LOGCHANNELID).sendMessage(timestamp + msg);
         } catch (Exception e)
         {
-            main.log("Something went wrong, while reporting that something went wrong. PERMISSIONS?!");
+            main.log("Something went wrong, while logging something. PERMISSIONS?!");
             main.log("**********BEGIN ERROR REPORT**********");
-            main.log(e.getLocalizedMessage());
-            for (StackTraceElement e2 : e.getStackTrace())
-            {
-                main.log(e2.toString());
-            }
+            e.printStackTrace(); //
             main.log("**********END ERROR REPORT**********");
+        }
+    }
+
+    @EventSubscriber
+    public void onCommand(MessageReceivedEvent e)
+    {
+        try
+        {
+
+            IMessage msg = e.getMessage();
+            IUser sdr = msg.getAuthor();
+            String nameOfSender = sdr.getNicknameForGuild(msg.getGuild()).isPresent() ? sdr.getNicknameForGuild(msg.getGuild()).get() : sdr.getName();
+            IChannel chnl = msg.getChannel();
+            String content = msg.getContent();
+            if (!content.startsWith("?"))
+            {
+                //Not a command.
+                return;
+            }
+            String command;
+            String arguments;
+            if (content.contains(" "))
+            {
+                command = content.substring(1, content.indexOf(" ")); //From the character after the '?' to the character before the first space.
+                arguments = content.trim().substring(content.indexOf(" ") + 1, content.length()); //From the character after the first space, to the end.
+            } else
+            {
+                command = content.substring(1, content.length());
+                arguments = "";
+            }
+            System.out.println("Recieved Command: " + command + " from user \"" + nameOfSender + "\"");
+            switch (command)
+            {
+                case "ping":
+                    String lagTime = ((Long) (System.currentTimeMillis() - msg.getCreationDate().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli())).toString();
+                    chnl.sendMessage("Pong! `" + lagTime + "ms lag`.");
+                    break;
+                default:
+                    chnl.sendMessage("Invalid command \"" + command + "\"");
+                    break;
+            }
+        } catch (Exception ex)
+        {
+            ex.printStackTrace();
         }
     }
 }
