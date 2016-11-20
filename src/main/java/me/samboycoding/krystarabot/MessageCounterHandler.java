cb2fd1f3a3d8d6c218a7a86dd8c1d191fa55be29
MrSnake <MrSnake@Tower.fritz.box>
2016-11-20 15:25:13 +0100

    message counter with issues

--- src/main/java/me/samboycoding/krystarabot/MessageCounterHandler.java
+++ src/main/java/me/samboycoding/krystarabot/MessageCounterHandler.java
@@ -7,69 +7,59 @@ import org.apache.commons.io.FileUtils;
 import org.json.JSONObject;
 
 /**
- * Handles the Code JSON file
+ * Handles the MessageCounter JSON file
  *
  * @author MrSnake
  */
-public class MessageCounterHandler
-{
-
-    public void MessageCounterHandler(String srvID) {
-        
-    }
+public class MessageCounterHandler {
     
-    File messageCounter = new File("messageCounter.json");
-    JSONObject messageCounterJSON;
+    File messageCounter = new File("MessageCounter.json");
+    JSONObject msgCounterJSON;
 
-    public void loadJSON()
-    {
-        try
-        {
+    //check is file is existent
+    public void loadJSON() {
+        try {
             main.log("Attempting to load message counter...");
-            if (messageCounter.exists())
-            {
+            if (messageCounter.exists()) { //file existent -> load
                 main.log("Existing message counter found, loading...");
                 loadFromJSON();
-            } else
-            {
+            } else { // file not existent -> create 
                 main.log("No message counter file found, creating a new one...");
                 messageCounter.createNewFile();
                 FileUtils.writeStringToFile(messageCounter, "{}", Charset.defaultCharset());
-                messageCounterJSON = new JSONObject();
+                msgCounterJSON = new JSONObject();
             }
             main.log("Success!");
-        } catch (IOException e)
-        {
+        } catch (IOException e) {
             main.log("Error loading/creating message counter file! Messages will NOT be counted...!");
             e.printStackTrace();
         }
     }
     
-    private void loadFromJSON()
-    {
-        try
-        {
+    //load the file content
+    private void loadFromJSON() {
+        try {
             String jsonRaw = FileUtils.readFileToString(messageCounter, Charset.defaultCharset());
-            messageCounterJSON = new JSONObject(jsonRaw);
+            msgCounterJSON = new JSONObject(jsonRaw);
             main.log("Succesfully loaded message counter from file!");
-        } catch (IOException ex)
-        {
+        } catch (IOException ex) {
             main.log("Error reading codes file!");
             ex.printStackTrace();
         }
     }
     
+    //count the messages for the user
     public void countMessage(String sdr) throws IOException {
-        if(messageCounterJSON.isNull(sdr)) {
+        if(msgCounterJSON.isNull(sdr)) { //if key with sender id is NULL create new JSONObject for new ueser 
             JSONObject newUser = new JSONObject();
             newUser.put("messages", 1);
-            messageCounterJSON.put(sdr, newUser);
-        } else {
-            JSONObject currentUser = messageCounterJSON.getJSONObject(sdr);
+            msgCounterJSON.put(sdr, newUser);
+        } else { //if key already exists, increase message counter
+            JSONObject currentUser = msgCounterJSON.getJSONObject(sdr);
             int messages = currentUser.getInt("messages");
             messages++;
             currentUser.put("messages", messages);
         }
-        FileUtils.writeStringToFile(messageCounter, messageCounterJSON.toString(4), Charset.defaultCharset());
+        FileUtils.writeStringToFile(messageCounter, msgCounterJSON.toString(4), Charset.defaultCharset());
     }
 }
