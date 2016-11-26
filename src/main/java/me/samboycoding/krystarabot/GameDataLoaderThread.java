00a63f59f5509cc2bf28331eb66516aa679ab8ef
Sam Byass <sam@whcuk.net>
2016-11-26 16:21:05 +0000

    Begin work on parsing web data. TODO: Spells (Easy), Check other items to make sure they work.

--- src/main/java/me/samboycoding/krystarabot/GameDataLoaderThread.java
+++ src/main/java/me/samboycoding/krystarabot/GameDataLoaderThread.java
@@ -9,7 +9,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Iterator;
 import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * Thread to load game data from http://ashtender.com/gems/tools/world.php.
@@ -33,7 +36,51 @@ public class GameDataLoaderThread implements Runnable {
             String body = IOUtils.toString(in, encoding);
             
             main.logToBoth("[Game Data Loader]  Passing to GameData class...");
-            main.data.readJSONFromString(body);
+            //main.data.readJSONFromString(body);
+            JSONObject rawData = new JSONObject(body);
+            
+            GameData.arrayClasses = rawData.getJSONArray("HeroClasses");
+            GameData.arrayKingdoms = rawData.getJSONArray("Kingdoms");
+            GameData.arrayTroops = rawData.getJSONArray("Troops");
+            
+            JSONArray traits = new JSONArray();
+            
+            //For each, calculate the new Health, Attack, Armor, and save any traits not yet known.
+            for (Iterator<Object> it = GameData.arrayTroops.iterator(); it.hasNext();)
+            {
+                JSONObject troop = (JSONObject) it.next();
+                JSONArray traitsForTroop = troop.getJSONArray("ParsedTraits");
+                for (Iterator<Object> it2 = traitsForTroop.iterator(); it2.hasNext();)
+                {
+                    JSONObject trait = (JSONObject) it2.next();
+                    JSONObject newTrait = new JSONObject();
+                    
+                    //Check if trait already known
+                    boolean known = false;
+                    for(Object knownTrait : traits)
+                    {
+                        JSONObject obj = (JSONObject) knownTrait;
+                        if(obj.getString("Name").equals(trait.getString("Name")))
+                        {
+                            known = true;
+                            break;
+                        }
+                    }
+                    if(known)
+                    {
+                        continue;
+                    }
+                    
+                    //Data slimming
+                    newTrait.put("Name", trait.get("Name"));
+                    newTrait.put("Description", trait.get("Description"));
+                    
+                    traits.put(newTrait);
+                }
+            }
+            
+            GameData.arrayTraits = traits;
+            
         } catch (IOException ex) {
             main.logToBoth("Unable to load Game Data: " + ex.getMessage());
         }
