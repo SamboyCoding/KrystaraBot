f26b28e7c2d64cba4aa1c43933518a122f60296a
Sam Byass <sam@whcuk.net>
2016-11-27 19:54:31 +0000

    Improvements to GameDataLoader (mainly comments)

--- src/main/java/me/samboycoding/krystarabot/GameDataLoaderThread.java
+++ src/main/java/me/samboycoding/krystarabot/GameDataLoaderThread.java
@@ -19,118 +19,166 @@ import org.json.JSONObject;
  *
  * @author Sam
  */
-public class GameDataLoaderThread implements Runnable {
+public class GameDataLoaderThread implements Runnable
+{
 
     @Override
-    public void run() {
-        try {
+    public void run()
+    {
+        try
+        {
+            long startConnectionTime = System.currentTimeMillis();
+
+            //Initial setup
             main.logToBoth("[Game Data Loader]  Attempting to load data from remote location...");
             URL url = new URL("http://ashtender.com/gems/tools/world.php");
+
+            //Make request
             main.logToBoth("[Game Data Loader]  Opening connection...");
             URLConnection con = url.openConnection();
-            main.logToBoth("[Game Data Loader]  Reading data...");
+
+            //Open stream to read data
+            main.logToBoth("[Game Data Loader] Reading data...");
             InputStream in = con.getInputStream();
             String encoding = con.getContentEncoding();
             encoding = encoding == null ? "UTF-8" : encoding;
-            main.logToBoth("[Game Data Loader]  Parsing body...");
-            String body = IOUtils.toString(in, encoding).replace("'", "\\'"); //Escape single quotes - org.json doesn't seem to like them?
 
-            //main.logToBoth("[Game Data Loader]  Passing to GameData class...");
-            //main.data.readJSONFromString(body);
+            //Read data -> string
+            main.logToBoth("[Game Data Loader] Retreiving body...");
+            String body = IOUtils.toString(in, encoding);
+
+            //Parse data
+            long finishDownloadTime = System.currentTimeMillis();
+            main.logToBoth("[Game Data Loader] Body retrieved (in " + (finishDownloadTime - startConnectionTime) + " milliseconds). Contains " + body.length() + " characters. Parsing -> JSONObject...");
             JSONObject rawData = new JSONObject(body);
 
+            //Just copy kingdoms array over as-is.
             main.logToBoth("[Game Data Loader] Loading Kingdoms...");
             GameData.arrayKingdoms = rawData.getJSONArray("Kingdoms");
             main.logToBoth("[Game Data Loader] Loaded " + GameData.arrayKingdoms.length() + " Kingdoms.");
 
+            //Copy classes over
             main.logToBoth("[Game Data Loader] Loading classes...");
             GameData.arrayClasses = rawData.getJSONArray("HeroClasses");
 
             JSONArray newClasses = new JSONArray();
 
-            for (Object c : GameData.arrayClasses) {
+            //Modify all hero classes, and add the kingdom name object.
+            for (Object c : GameData.arrayClasses)
+            {
                 JSONObject hClass = (JSONObject) c;
                 int kingdomId = hClass.getInt("KingdomId");
                 boolean found = false;
-                for (Object k : GameData.arrayKingdoms) {
+
+                //Loop through all kingdoms until we find one with the correct id.
+                for (Object k : GameData.arrayKingdoms)
+                {
                     JSONObject kingdom = (JSONObject) k;
                     int id = kingdom.getInt("Id");
-                    if (kingdomId == id) {
+                    if (kingdomId == id)
+                    {
                         found = true;
                         hClass.put("Kingdom", kingdom.get("Name").toString());
                         break;
                     }
                 }
 
-                if (!found) {
+                if (!found)
+                {
                     main.logToBoth("[Game Data Loader] Could not load kingdom info for Hero Class \"" + hClass.getString("Name") + "\"! It will not be available!");
                     continue;
                 }
 
                 newClasses.put(hClass);
             }
+
+            //Save modified class data.
             GameData.arrayClasses = newClasses;
             main.logToBoth("[Game Data Loader] Loaded " + GameData.arrayClasses.length() + " Hero Classes.");
+
+            //Copy troop array over.
             main.logToBoth("[Game Data Loader] Loading Troops");
             GameData.arrayTroops = rawData.getJSONArray("Troops");
 
             JSONArray modifiedTroops = new JSONArray();
 
-            for (Object trp : GameData.arrayTroops) {
+            //Fill in kingdom of origin for each troop
+            for (Object trp : GameData.arrayTroops)
+            {
                 JSONObject trp2 = (JSONObject) trp;
                 int id = trp2.getInt("Id");
                 boolean found = false;
 
-                for (Object kng : GameData.arrayKingdoms) {
+                //Loop through all kingdoms until we find the one containing our troop.
+                for (Object kng : GameData.arrayKingdoms)
+                {
                     JSONObject kngdom = (JSONObject) kng;
 
-                    for (Iterator<Object> it = kngdom.getJSONArray("TroopIds").iterator(); it.hasNext();) {
+                    //Loop through all troops - check if the one we want is in there.
+                    for (Iterator<Object> it = kngdom.getJSONArray("TroopIds").iterator(); it.hasNext();)
+                    {
                         int troopId = (int) it.next();
-                        if (troopId == id) {
+                        if (troopId == id)
+                        {
                             found = true;
                             trp2.put("Kingdom", kngdom.getString("Name"));
                             break;
                         }
                     }
 
-                    if (found) {
+                    if (found)
+                    {
                         break; //We've found the troop - don't continue to loop through the kingdoms
                     }
                 }
 
-                if (!trp2.has("Kingdom")) {
-                    main.logToBoth("[Game Data Loader] ERROR! Could not find kingdom for troop \"" + trp2.getString("Name") + "\"! It will not be available!");
+                if (!trp2.has("Kingdom"))
+                {
+                    main.logToBoth("[Game Data Loader] ERROR! Could not find kingdom for troop \"" + trp2.getString("Name") + "\" (with reference name \"" + trp2.getString("ReferenceName") + "\")! It will not be available!");
                     continue; //Do not add
                 }
 
                 modifiedTroops.put(trp2); //Add to new troops array
             }
 
+            //Save modified troops
             GameData.arrayTroops = modifiedTroops;
-
             main.logToBoth("[Game Data Loader] Loaded " + GameData.arrayTroops.length() + " Troops.");
 
+            //Copy traits over
             main.logToBoth("[Game Data Loader] Loading Traits...");
             JSONArray traits = new JSONArray();
 
             //For each troop, save any traits not yet known.
-            for (Iterator<Object> it = GameData.arrayTroops.iterator(); it.hasNext();) {
+            //Loop through all troops
+            for (Iterator<Object> it = GameData.arrayTroops.iterator(); it.hasNext();)
+            {
                 JSONObject troop = (JSONObject) it.next();
                 JSONArray traitsForTroop = troop.getJSONArray("ParsedTraits");
-                for (Iterator<Object> it2 = traitsForTroop.iterator(); it2.hasNext();) {
+
+                //Loop through its traits
+                for (Iterator<Object> it2 = traitsForTroop.iterator(); it2.hasNext();)
+                {
                     JSONObject trait = (JSONObject) it2.next();
                     JSONObject newTrait = new JSONObject();
 
                     //Check if trait already known
                     boolean known = false;
-                    for (Object knownTrait : traits) {
+
+                    //Loop through all known traits
+                    for (Object knownTrait : traits)
+                    {
                         JSONObject obj = (JSONObject) knownTrait;
-                        if (obj.getString("Name").equals(trait.getString("Name"))) {
+                        if (obj.getString("Name").equals(trait.getString("Name")))
+                        {
                             known = true;
                             break;
                         }
                     }
-                    if (known) {
+
+                    //Move on if known
+                    if (known)
+                    {
                         continue;
                     }
 
@@ -138,28 +186,40 @@ public class GameDataLoaderThread implements Runnable {
                     newTrait.put("Name", trait.get("Name"));
                     newTrait.put("Description", trait.get("Description"));
 
+                    //Add to known traits
                     traits.put(newTrait);
                 }
             }
-            
-            //And again, for the hero class traits.
-            for (Iterator<Object> it = GameData.arrayClasses.iterator(); it.hasNext();) {
+
+            //Do the same again, for the hero class traits.
+            for (Iterator<Object> it = GameData.arrayClasses.iterator(); it.hasNext();)
+            {
                 JSONObject hClass = (JSONObject) it.next();
                 JSONArray traitsForClass = hClass.getJSONArray("ParsedTraits");
-                for (Iterator<Object> it2 = traitsForClass.iterator(); it2.hasNext();) {
+
+                //Loop through all class traits
+                for (Iterator<Object> it2 = traitsForClass.iterator(); it2.hasNext();)
+                {
                     JSONObject trait = (JSONObject) it2.next();
                     JSONObject newTrait = new JSONObject();
 
                     //Check if trait already known
                     boolean known = false;
-                    for (Object knownTrait : traits) {
+
+                    //Loop through all known traits
+                    for (Object knownTrait : traits)
+                    {
                         JSONObject obj = (JSONObject) knownTrait;
-                        if (obj.getString("Name").equals(trait.getString("Name"))) {
+                        if (obj.getString("Name").equals(trait.getString("Name")))
+                        {
                             known = true;
                             break;
                         }
                     }
-                    if (known) {
+
+                    //Move on if known
+                    if (known)
+                    {
                         continue;
                     }
 
@@ -167,18 +227,22 @@ public class GameDataLoaderThread implements Runnable {
                     newTrait.put("Name", trait.get("Name"));
                     newTrait.put("Description", trait.get("Description"));
 
+                    //Add to known traits
                     traits.put(newTrait);
                 }
             }
-            
 
+            //Save modified traits
             GameData.arrayTraits = traits;
             main.logToBoth("[Game Data Loader] Loaded " + GameData.arrayTraits.length() + " traits.");
 
+            //Create empty spells array
             main.logToBoth("[Game Data Loader] Loading spells...");
             JSONArray newSpells = new JSONArray();
+
             //Go through all troops, and add spells.
-            for (Object t : GameData.arrayTroops) {
+            for (Object t : GameData.arrayTroops)
+            {
                 //Gather raw data
                 JSONObject trp = (JSONObject) t;
                 JSONObject spellInfo = trp.getJSONObject("Spell");
@@ -197,12 +261,17 @@ public class GameDataLoaderThread implements Runnable {
                 //Add to new spells array.
                 newSpells.put(spell);
             }
+
+            //Copy over new array
             GameData.arraySpells = newSpells;
             main.logToBoth("[Game Data Loader] Loaded " + GameData.arraySpells.length() + " spells");
 
-            main.logToBoth("[Game Data Loader] Finished loading game data.");
+            //Logging.
+            long finishParseTime = System.currentTimeMillis();
+            main.logToBoth("[Game Data Loader] Finished loading game data (in " + (finishParseTime - startConnectionTime) + " milliseconds - " + (finishParseTime - finishDownloadTime) + " milliseconds since download finished).");
             GameData.dataLoaded = true;
-        } catch (IOException ex) {
+        } catch (IOException ex)
+        {
             main.logToBoth("Unable to load Game Data: " + ex.getMessage());
         }
     }
@@ -215,23 +284,31 @@ public class GameDataLoaderThread implements Runnable {
      * @return A formatted string containing the text the {1} placeholder should
      * hold.
      */
-    private String getMagicValue(JSONObject firstStep) {
+    private String getMagicValue(JSONObject firstStep)
+    {
         //get the magic value for spell descriptions
         String magicDesc = "Magic";
-        if (firstStep.getBoolean("Primarypower")) {
-            if (firstStep.getInt("SpellPowerMultiplier") == 0) {
+        if (firstStep.getBoolean("Primarypower"))
+        {
+            if (firstStep.getInt("SpellPowerMultiplier") == 0)
+            {
                 return "" + firstStep.getInt("Amount");
             }
-            if (firstStep.getInt("Amount") != 0) {
+            if (firstStep.getInt("Amount") != 0)
+            {
                 magicDesc = magicDesc + "+" + firstStep.getInt("Amount");
             }
-            if (firstStep.getInt("SpellPowerMultiplier") != 1) {
-                if (!magicDesc.equals("Magic")) {
+            if (firstStep.getInt("SpellPowerMultiplier") != 1)
+            {
+                if (!magicDesc.equals("Magic"))
+                {
                     magicDesc = "(" + magicDesc + ")";
                 }
-                if (firstStep.getInt("SpellPowerMultiplier") < 1) {
+                if (firstStep.getInt("SpellPowerMultiplier") < 1)
+                {
                     magicDesc = magicDesc + " / " + (1 / firstStep.getInt("SpellPowerMultiplier"));
-                } else {
+                } else
+                {
                     magicDesc = magicDesc + " x " + firstStep.getInt("SpellPowerMultiplier");
                 }
             }
