f42d92016216b91dcb02f772c559446404947d87
Sam Byass <sam@whcuk.net>
2016-12-14 16:30:39 +0000

    Modify Quiz

--- src/main/java/me/samboycoding/krystarabot/command/QuizCommand.java
+++ src/main/java/me/samboycoding/krystarabot/command/QuizCommand.java
@@ -1,6 +1,7 @@
 package me.samboycoding.krystarabot.command;
 
 import java.util.ArrayList;
+import me.samboycoding.krystarabot.GameData;
 import static me.samboycoding.krystarabot.command.CommandType.GOW;
 import me.samboycoding.krystarabot.main;
 import sx.blah.discord.handle.obj.IChannel;
@@ -21,13 +22,18 @@ public class QuizCommand extends KrystaraCommand {
     @Override
     public void handleCommand(IUser sdr, IChannel chnl, IMessage msg, ArrayList<String> arguments, String argsFull) throws Exception
     {
+        if(!GameData.dataLoaded)
+        {
+            chnl.sendMessage("Unfortunately, the data is loaded yet. Please try again later, and if the problem persists, ask a bot dev to do `?reload-data`. Thanks!");
+            return;
+        }
         main.quizH.initializeQuiz(chnl.getGuild(), sdr, chnl);
     }
 
     @Override
     public String getHelpText()
     {
-        return "Starts, or signs you up for, a quiz.";
+        return "Starts a quiz.";
     }
 
     @Override
