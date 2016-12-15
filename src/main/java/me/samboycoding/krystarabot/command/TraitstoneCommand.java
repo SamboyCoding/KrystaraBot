cadc42619721941e4fdd5b22fa88e6eb76324a2e
Emily Ash <ashenbloom@gmail.com>
2016-12-14 18:45:54 -0800

    Refactored ?traitstone a bit, added hero classes to the list of dependencies.

--- src/main/java/me/samboycoding/krystarabot/command/TraitstoneCommand.java
+++ src/main/java/me/samboycoding/krystarabot/command/TraitstoneCommand.java
@@ -9,6 +9,7 @@ import static me.samboycoding.krystarabot.command.CommandType.MOD;
 import me.samboycoding.krystarabot.main;
 import me.samboycoding.krystarabot.utilities.Utilities;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IGuild;
@@ -22,6 +23,9 @@ import sx.blah.discord.handle.obj.IUser;
  */
 public class TraitstoneCommand extends KrystaraCommand
 {
+    /**
+     * Represents the rarity of a given traitstone.
+     */
     private static enum Rarity
     {
         Minor,
@@ -31,6 +35,9 @@ public class TraitstoneCommand extends KrystaraCommand
         Celestial
     }
     
+    /**
+     * Represents the color of a given traitstone.
+     */
     private static enum Color
     {
         Blue("mana_blue"),
@@ -40,6 +47,9 @@ public class TraitstoneCommand extends KrystaraCommand
         Purple("mana_purple"),
         Brown("mana_brown");
         
+        /**
+         * Gets the emoji associated with the traitstone color.
+         */
         public final String emoji;
         
         private Color(String e)
@@ -48,6 +58,10 @@ public class TraitstoneCommand extends KrystaraCommand
         }
     }
     
+    /**
+     * Represents a traitstone type.
+     * The order is important; do not modify.
+     */
     private static enum Traitstone
     {
         MinorWater("Minor Water Traitstone",        Rarity.Minor,     new Color[] { Color.Blue }),
@@ -56,18 +70,21 @@ public class TraitstoneCommand extends KrystaraCommand
         MinorWind("Minor Wind Traitstone",          Rarity.Minor,     new Color[] { Color.Yellow }),
         MinorMagic("Minor Magic Traitstone",        Rarity.Minor,     new Color[] { Color.Purple }),
         MinorEarth("Minor Earth Traitstone",        Rarity.Minor,     new Color[] { Color.Brown }),
+        
         MajorWater("Major Water Traitstone",        Rarity.Major,     new Color[] { Color.Blue }),
         MajorNature("Major Nature Traitstone",      Rarity.Major,     new Color[] { Color.Green }),
         MajorFire("Major Fire Traitstone",          Rarity.Major,     new Color[] { Color.Red }),
         MajorWind("Major Wind Traitstone",          Rarity.Major,     new Color[] { Color.Yellow }),
         MajorMagic("Major Magic Traitstone",        Rarity.Major,     new Color[] { Color.Purple }),
         MajorEarth("Major Earth Traitstone",        Rarity.Major,     new Color[] { Color.Brown }),
+        
         RunicWater("Runic Water Traitstone",        Rarity.Runic,     new Color[] { Color.Blue }),
         RunicNature("Runic Nature Traitstone",      Rarity.Runic,     new Color[] { Color.Green }),
         RunicFire("Runic Fire Traitstone",          Rarity.Runic,     new Color[] { Color.Red }),
         RunicWind("Runic Wind Traitstone",          Rarity.Runic,     new Color[] { Color.Yellow }),
         RunicMagic("Runic Magic Traitstone",        Rarity.Runic,     new Color[] { Color.Purple }),
         RunicEarth("Runic Earth Traitstone",        Rarity.Runic,     new Color[] { Color.Brown }),
+        
         ArcaneStoic("Arcane Stoic Traitstone",      Rarity.Arcane,    new Color[] { Color.Blue }),
         ArcaneSwamp("Arcane Swamp Traitstone",      Rarity.Arcane,    new Color[] { Color.Blue, Color.Green }),
         ArcaneBlood("Arcane Blood Traitstone",      Rarity.Arcane,    new Color[] { Color.Blue, Color.Red }),
@@ -89,6 +106,7 @@ public class TraitstoneCommand extends KrystaraCommand
         ArcaneDeath("Arcane Death Traitstone",      Rarity.Arcane,    new Color[] { Color.Purple }),
         ArcaneSkull("Arcane Skull Traitstone",      Rarity.Arcane,    new Color[] { Color.Purple, Color.Brown }),
         ArcaneDeep("Arcane Deep Traitstone",        Rarity.Arcane,    new Color[] { Color.Brown }),
+        
         Celestial("Celestial Traitstone",           Rarity.Celestial, new Color[] { Color.Blue, Color.Green, Color.Red, Color.Yellow, Color.Purple, Color.Brown });
         
         public final String prettyName;
@@ -151,17 +169,24 @@ public class TraitstoneCommand extends KrystaraCommand
         Utilities.sendLargeMessage(chnl, output);
     }
 
+    /**
+     * Parses the arguments list for a list of traitstones that match.
+     * @param arguments User-supplied arguments.
+     * @return The list of traitstones that match the arguments.
+     */
     private ArrayList<Traitstone> parseArguments(ArrayList<String> arguments)
     {
-        // Try to find a traitstone by name
         ArrayList<Traitstone> traitstones = new ArrayList<>();
         ArrayList<Color> colorArgs = new ArrayList<>();
         ArrayList<String> nameArgs = new ArrayList<>();
         Rarity rarityArg = null;
+
         for (String arg : arguments)
         {
             boolean found = false;
             arg = arg.toLowerCase();
+            
+            // If the argument exactly matches a color, track that color
             for (Color color : Color.values())
             {
                 if (color.name().toLowerCase().equals(arg))
@@ -172,6 +197,8 @@ public class TraitstoneCommand extends KrystaraCommand
                 }
             }
             
+            // If the argument exactly matches a rarity, and we don't already
+            // have a rarity, track that rarity
             if (!found && (rarityArg == null))
             {
                 for (Rarity rarity : Rarity.values())
@@ -185,18 +212,26 @@ public class TraitstoneCommand extends KrystaraCommand
                 }
             }
             
+            // If neither is the case, assume the string is a name match
             if (!found)
             {
                 nameArgs.add(arg);
             }
         }
+        
+        // If no rarity was specified, assume one
         if (rarityArg == null)
         {
             rarityArg = Rarity.Arcane;
         }
+        
+        // Sort the user's color choices
         colorArgs.sort((c1, c2) -> c1.compareTo(c2));
+
+        // Loop over each traitstone type looking for matches
         for (Traitstone traitstone : Traitstone.values())
         {
+            // Match rarity
             if (traitstone.rarity != rarityArg)
             {
                 continue;
@@ -204,15 +239,11 @@ public class TraitstoneCommand extends KrystaraCommand
             
             boolean found = true;
 
+            // Match text in the name
+            String name = traitstone.prettyName.toLowerCase();
             for (String arg : nameArgs)
             {
-                boolean foundThisArg = false;
-                if (traitstone.prettyName.toLowerCase().contains(arg))
-                {
-                    foundThisArg = true;
-                }
-                
-                if (!foundThisArg)
+                if (!name.contains(arg))
                 {
                     found = false;
                     break;
@@ -234,28 +265,108 @@ public class TraitstoneCommand extends KrystaraCommand
             
             if (found)
             {
+                // Passed all criteria, so add the traitstone
                 traitstones.add(traitstone);
             }
         }
         return traitstones;
     }
     
+    /**
+     * Gets the informational text associated with this traitstone.
+     * @param traitstone The traitstone to get text for.
+     * @param channel The current channel.
+     * @return The informational text.
+     */
     private String getTraitstoneInfoText(Traitstone traitstone, IChannel channel)
     {
         int id = traitstone.getId();
         ArrayList<String> kingdomNames = new ArrayList<>();
-                
+        
+        // Look for kingdoms that have this traitstone set as their ExploreRune
         for (Object oKingdom : GameData.arrayKingdoms)
         {
             JSONObject kingdom = (JSONObject)oKingdom;
             if (kingdom.has("ExploreRune") && !kingdom.isNull("ExploreRune") && kingdom.getInt("ExploreRune") == id)
             {
+                // Found one, add the name to the list
                 kingdomNames.add(kingdom.getString("Name"));
             }
         }
         
+        // Look for troops and classes that have this traitstone as a cost, and accumulate costs
+        HashMap<JSONObject, Integer> troopNeededMap = getTraitstoneNeededMap(GameData.arrayTroops, id);
+        int totalTroopAmountNeeded = getTraitstoneNeededCount(troopNeededMap);
+        ArrayList<String> troopNeededNames = getTraitstoneNeededNames(troopNeededMap);
+
+        HashMap<JSONObject, Integer> classNeededMap = getTraitstoneNeededMap(GameData.arrayClasses, id);
+        int totalClassAmountNeeded = getTraitstoneNeededCount(classNeededMap);
+        ArrayList<String> classNeededNames = getTraitstoneNeededNames(classNeededMap);
+        
+        String result = "";
+
+        result += "**" + traitstone.prettyName + "** ";
+
+        // Transform color array into array of emoji strings, and then join
+        IGuild guild = channel.getGuild();
+        String[] colors = Arrays.stream(traitstone.colors).map(c -> guild.getEmojiByName(c.emoji).toString()).toArray(String[]::new);
+        result += String.join(" ", colors) + "\n";
+
+        
+        // Add relevant output if found
+        if (!kingdomNames.isEmpty())
+        {
+            result += "Found in: " + String.join(", ", kingdomNames) + "\n";
+        }
+        if (!troopNeededNames.isEmpty())
+        {
+            final int LIMIT = 50;
+            int size = troopNeededNames.size();
+            
+            if (size > LIMIT)
+            {
+                String extraText = "... (" + (size - LIMIT) + " more)";
+                troopNeededNames.subList(LIMIT, size).clear();
+                troopNeededNames.add(extraText);
+            }
+            result += "\nNeeded by troops: " + String.join(", ", troopNeededNames) + "\n";
+        }
+        if (!classNeededNames.isEmpty())
+        {
+            result += "\nNeeded by classes: " + String.join(", ", classNeededNames) + "\n";
+        }
+        result += "\nTotal needed: " + (totalTroopAmountNeeded + totalClassAmountNeeded) + "\n\n";
+
+        return result;
+    }
+
+    private ArrayList<String> getTraitstoneNeededNames(HashMap<JSONObject, Integer> troopNeededMap)
+    {
+        ArrayList<String> troopNeededNames = new ArrayList<>();
+        for (Object oTroop : troopNeededMap.keySet())
+        {
+            JSONObject troop = (JSONObject)oTroop;
+            int amountNeeded = troopNeededMap.get(troop);
+            troopNeededNames.add(troop.getString("Name") + " (" + amountNeeded + ")");
+        }
+        troopNeededNames.sort((s1, s2) -> s1.compareTo(s2));
+        return troopNeededNames;
+    }
+
+    private int getTraitstoneNeededCount(HashMap<JSONObject, Integer> troopNeededMap)
+    {
+        int totalAmountNeeded = 0;
+        for (int amountNeeded : troopNeededMap.values())
+        {
+            totalAmountNeeded += amountNeeded;
+        }
+        return totalAmountNeeded;
+    }
+
+    private HashMap<JSONObject, Integer> getTraitstoneNeededMap(JSONArray troops, int id) throws JSONException
+    {
         HashMap<JSONObject, Integer> troopNeededMap = new HashMap<>();
-        for (Object oTroop : GameData.arrayTroops)
+        for (Object oTroop : troops)
         {
             JSONObject troop = (JSONObject)oTroop;
             JSONArray traitTable = troop.getJSONObject("TraitTable").getJSONArray("Runes");
@@ -267,6 +378,7 @@ public class TraitstoneCommand extends KrystaraCommand
                     JSONObject traitCost = (JSONObject)oTraitCost;
                     if (traitCost.getInt("Id") == id)
                     {
+                        // Add the trait's costs to the running total
                         int needed = troopNeededMap.getOrDefault(troop, 0);
                         needed += traitCost.getInt("Required");
                         troopNeededMap.put(troop, needed);
@@ -274,36 +386,7 @@ public class TraitstoneCommand extends KrystaraCommand
                 }
             }
         }
-        
-        ArrayList<String> troopNeededNames = new ArrayList<>();
-        if (!troopNeededMap.isEmpty())
-        {
-            JSONObject[] oTroops = troopNeededMap.keySet().toArray(new JSONObject[0]);
-            ArrayList<JSONObject> troops = new ArrayList<>(Arrays.asList(oTroops));
-            troops.sort((t1, t2) -> t1.getString("Name").compareTo(t2.getString("Name")));
-            
-            for (JSONObject troop : troops)
-            {
-                troopNeededNames.add(troop.getString("Name") + " (" + troopNeededMap.get(troop) + ")");
-            }
-        }
-        
-        String result = "**" + traitstone.prettyName + "**\n";
-
-        IGuild guild = channel.getGuild();
-        String[] colors = Arrays.stream(traitstone.colors).map(c -> guild.getEmojiByName(c.emoji).toString()).toArray(String[]::new);
-        result += String.join(" ", colors) + "\n";
-
-        if (!kingdomNames.isEmpty())
-        {
-            result += "Found in: " + String.join(", ", kingdomNames) + "\n";
-        }
-        if (!troopNeededNames.isEmpty())
-        {
-            result += "Needed for: " + String.join(", ", troopNeededNames) + "\n";
-        }
-
-        return result;
+        return troopNeededMap;
     }
 
     @Override
