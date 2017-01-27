cf13b66ab0c2566b8ef0ca8f9efa0a218147d542
Sam Byass <sam@whcuk.net>
2017-01-27 20:03:12 +0000

    Back to cleverbot - just needed to update the api 0_0

--- src/main/java/me/samboycoding/krystarabot/main.java
+++ src/main/java/me/samboycoding/krystarabot/main.java
@@ -83,7 +83,7 @@ public class main
         logToBoth("Initializing Intelligent Talking...");
         ChatterBotFactory factory = new ChatterBotFactory();
 
-        ChatterBot bot1 = factory.create(ChatterBotType.JABBERWACKY);
+        ChatterBot bot1 = factory.create(ChatterBotType.CLEVERBOT);
         cleverBot = bot1.createSession();
 
         logToBoth("Intelligent talking loaded!");
